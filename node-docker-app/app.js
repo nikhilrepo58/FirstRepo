e68a1d6b649b7f29dc3f41d9d1c34180aecc1f74
NikhilRepo <niksaggarwal58@gmail.com>
2026-05-03 11:08:32 +0000

    cleandocker

--- node-docker-app/app.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.end('Hello from WSL Docker ');
-});
-
-server.listen(3000, () => {
-  console.log('Running on port 3000');
-});
